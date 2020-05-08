89c347f5ba06b720a9d179bdc7d8b212b3784517
naiduvishal13 <naiduvishal13@gmail.com>
2020-05-08 16:23:04 -0600

    Added max square distance limit

--- Assets/Scripts/InputMapper.cs
+++ Assets/Scripts/InputMapper.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class InputMapper : MonoBehaviour
 {
+    [Header("Config")]
+    public float MaxSquareDistance = 1.5f;
+
     [Header("Hand References")]
     public GameObject LeftHand;
     public GameObject RightHand;
@@ -24,6 +27,26 @@ public class InputMapper : MonoBehaviour
 
     void Update()
     {
-        
+        float RestingSquareDistance = Mathf.Clamp(
+            (LeftHand.transform.localPosition - RestingSpotLeft.transform.localPosition).magnitude *
+            (RightHand.transform.localPosition - RestingSpotRight.transform.localPosition).magnitude,
+            0.001f, MaxSquareDistance);
+
+        float JoySquareDistance = Mathf.Clamp(
+            (LeftHand.transform.localPosition - JoySpotLeft.transform.localPosition).magnitude *
+            (RightHand.transform.localPosition - JoySpotRight.transform.localPosition).magnitude,
+            0.001f, MaxSquareDistance);
+
+        float FearSquareDistance = Mathf.Clamp(
+            (LeftHand.transform.localPosition - FearSpotLeft.transform.localPosition).magnitude *
+            (RightHand.transform.localPosition - FearSpotRight.transform.localPosition).magnitude,
+            0.001f, MaxSquareDistance);
+
+        float SorrowSquareDistance = Mathf.Clamp(
+            (LeftHand.transform.localPosition - SorrowSpotLeft.transform.localPosition).magnitude *
+            (RightHand.transform.localPosition - SorrowSpotRight.transform.localPosition).magnitude,
+            0.001f, MaxSquareDistance);
+
+        Debug.LogError("Rest = " + RestingSquareDistance + " | Joy = " + JoySquareDistance + " | Fear = " + FearSquareDistance + " | Sorrow = " + SorrowSquareDistance);
     }
 }
