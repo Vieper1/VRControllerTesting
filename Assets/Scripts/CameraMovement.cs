9bd65a518017b276ce5bc1aec85bad959b573c5d
naiduvishal13 <naiduvishal13@gmail.com>
2020-05-08 15:14:26 -0600

    Initial commit

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [Header("Config")]
+    public float MouseSensitivity = 1.0f;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        float rotateHorizontal = Input.GetAxis("Mouse X");
+        float rotateVertical = Input.GetAxis("Mouse Y");
+        
+        transform.RotateAround(transform.position, Vector3.up, rotateHorizontal * MouseSensitivity);
+        transform.RotateAround(transform.position, -transform.right, rotateVertical * MouseSensitivity);
+    }
+}
---
+++ Assets/Scripts/HandMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandMovement : MonoBehaviour
+{
+    [Header("Config")]
+    public float MovementSpeed = 1.0f;
+    public KeyCode Forward;
+    public KeyCode Backward;
+    public KeyCode Left;
+    public KeyCode Right;
+    public KeyCode Up;
+    public KeyCode Down;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        float combinedSpeed = MovementSpeed * Time.deltaTime;
+        if (Input.GetKey(Forward))
+            transform.localPosition = Vector3.forward * combinedSpeed;
+        if (Input.GetKey(Backward))
+            transform.localPosition += Vector3.back * combinedSpeed;
+
+        if (Input.GetKey(Left))
+            transform.localPosition += Vector3.left * combinedSpeed;
+        if (Input.GetKey(Right))
+            transform.localPosition += Vector3.right * combinedSpeed;
+
+        if (Input.GetKey(Up))
+            transform.localPosition += Vector3.up * combinedSpeed;
+        if (Input.GetKey(Down))
+            transform.localPosition += Vector3.down * combinedSpeed;
+    }
+}
